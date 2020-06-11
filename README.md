8cc35d7e920e28758e6c8612a11813a49dbb1ec8
freddieRv <federico.ram.v@gmail.com>
2020-06-11 15:42:31 -0500

    removed openCV dependency - updated readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# DBSCAN
+
+A simple C++ implementation of DBSCAN algorithm on 3D space using euclidean distance.
+
+Build with cmake and make.
+
+`dbscan_test.cpp` uses a CSV file as input for the data
--- dbscan_test.cpp
+++ dbscan_test.cpp
@@ -1,18 +1,15 @@
 #include <DBSCAN.h>
 
 #include <cstdlib>
-#include <ctime>
 #include <fstream>
 #include <iostream>
 #include <map>
-#include <sstream>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <sstream>
 
-#include <opencv2/core/core.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-
-cv::Mat* loadMatrixFromCSVFile(std::string);
+std::vector<dbscan::_3DPoint*>* loadMatrixFromCSVFile(std::string);
 std::vector<std::string> splitLine(std::string, char);
 
 int main(int argc, char const *argv[]) {
@@ -23,66 +20,21 @@ int main(int argc, char const *argv[]) {
 
     int minPts = std::atoi(argv[3]);
     double eps = std::atof(argv[2]);
-    std::vector<dbscan::_3DPoint*> points;
+    std::vector<dbscan::_3DPoint*>* points = loadMatrixFromCSVFile(argv[1]);
 
     std::cout << "minPts: " << minPts << std::endl;
     std::cout << "eps: " << eps << std::endl;
 
-    cv::Mat* raw_points = UTILS::loadMatrixFromCSVFile(argv[1]);
-
-    for (size_t i = 0; i < raw_points->cols; i++) {
-        points.push_back(
-            new dbscan::_3DPoint(
-                raw_points->at<double>(0, i),
-                raw_points->at<double>(1, i),
-                raw_points->at<double>(2, i)
-            )
-        );
-    }
-
-    dbscan::DBSCAN db(eps, minPts, &points);
+    dbscan::DBSCAN db(eps, minPts, points);
     db.run();
 
     std::cout << "Done!" << std::endl;
 
-    std::map<int, std::vector<int>> color_map = std::map<int, std::vector<int>>();
-    std::ofstream outf = std::ofstream();
-
-    srand(time(NULL));
-    outf.open("test_color_mappign.csv");
-
-    for (size_t i = 0; i < points.size(); i++) {
-        // std::cout << *points[i] << std::endl;
-
-        if (points[i]->m_label == dbscan::Label::CORE) {
-
-            std::map<int, std::vector<int>>::iterator label_color = color_map.find(points[i]->m_clusterID);
-            if (label_color == color_map.end()) {
-                std::vector<int> color(3);
-                color[0] = rand() % 255;
-                color[1] = rand() % 255;
-                color[2] = rand() % 255;
-                color_map[points[i]->m_clusterID] = color;
-            }
-
-            outf << color_map[points[i]->m_clusterID][0] << ","
-                 << color_map[points[i]->m_clusterID][1] << ","
-                 << color_map[points[i]->m_clusterID][2] << "\n";
-
-        } else {
-            outf << 0 << ","
-                 << 0 << ","
-                 << 0 << "\n";
-        }
-    }
-
-    outf.close();
-
     return 0;
 }
 
-cv::Mat* loadMatrixFromCSVFile(std::string file_path) {
-    cv::Mat* mat = nullptr;
+std::vector<dbscan::_3DPoint*>* loadMatrixFromCSVFile(std::string file_path) {
+    std::vector<dbscan::_3DPoint*>* mat = nullptr;
 
     if (file_path != "") {
         std::ifstream inf;
@@ -102,19 +54,19 @@ cv::Mat* loadMatrixFromCSVFile(std::string file_path) {
             inf.clear();
             inf.seekg(0, std::ios::beg);
 
-            mat = new cv::Mat(3, count, CV_64FC1);
+            mat = new std::vector<dbscan::_3DPoint*>;
 
-            int line_count = 0;
             while (getline(inf, line)) {
                 splited = splitLine(line, ',');
-
                 if (splited.size() == 3) {
-                    mat->at<double>(0, line_count) = std::stod(splited[0]);
-                    mat->at<double>(1, line_count) = std::stod(splited[1]);
-                    mat->at<double>(2, line_count) = std::stod(splited[2]);
+                    mat->push_back(
+                        new dbscan::_3DPoint(
+                            std::stod(splited[0]),
+                            std::stod(splited[1]),
+                            std::stod(splited[2])
+                        )
+                    );
                 }
-
-                line_count++;
             }
 
             inf.close();
